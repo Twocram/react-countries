d8f42f89a9911e4989ec89d80f0c9cb99ba670e5
Twocram <2002maslovartem@gmail.com>
2022-05-21 22:29:09 +0500

    created theme switcher script

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { IoMoon, IoMoonOutline } from "react-icons/io5";
 import { Container } from "./Container";
+import { useEffect, useState } from "react";
 
 const HeaderEl = styled.header``;
 
@@ -13,12 +14,18 @@ const Title = styled.a.attrs({
 const ModeSwitcher = styled.div``;
 
 const Header = () => {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  }, [theme]);
   return (
     <HeaderEl>
       <Container>
         <Wrapper>
           <Title>Where is the world ?</Title>
-          <ModeSwitcher>
+          <ModeSwitcher onClick={toggleTheme}>
             <IoMoon /> Light Theme
           </ModeSwitcher>
         </Wrapper>
