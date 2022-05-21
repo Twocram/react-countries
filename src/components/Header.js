ccd709aa69331e0cb8bce48cb149be28dc90e947
Twocram <2002maslovartem@gmail.com>
2022-05-21 22:19:08 +0500

    created Header basic structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import { IoMoon, IoMoonOutline } from "react-icons/io5";
+import { Container } from "./Container";
+
+const HeaderEl = styled.header``;
+
+const Wrapper = styled.div``;
+
+const Title = styled.a.attrs({
+  href: "/",
+})``;
+
+const ModeSwitcher = styled.div``;
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderEl>
+      <Container>
+        <Wrapper>
+          <Title>Where is the world ?</Title>
+          <ModeSwitcher>
+            <IoMoon /> Light Theme
+          </ModeSwitcher>
+        </Wrapper>
+      </Container>
+    </HeaderEl>
+  );
 };
 
 export default Header;
