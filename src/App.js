01b8e335672ff28463e9076aeef3fe6bf3bee7a6
Twocram <2002maslovartem@gmail.com>
2022-05-22 17:01:26 +0500

    removed empty space

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from './components/Header';
 import { Main } from './components/Main';
-
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Details from './pages/Details';
