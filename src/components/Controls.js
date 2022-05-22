4cbe6b670feecf129c96203eae0b16685ced3fe5
Twocram <2002maslovartem@gmail.com>
2022-05-22 12:00:05 +0500

    added options and CustomSelect with attributes

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,11 +1,28 @@
 import { useState } from 'react';
+import { CustomSelect } from './CustomSelect';
 import Search from './Search';
+
+const options = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'America', label: 'America' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'Oceania', label: 'Oceania' },
+];
 
 const Controls = () => {
   const [search, setSearch] = useState('');
+  const [region, setRegion] = useState('');
   return (
     <div>
       <Search search={search} setSearch={setSearch} />
+      <CustomSelect
+        options={options}
+        placeholder='Filter by Region'
+        isSearchable={false}
+        value={region}
+        onChange={setRegion}
+      />
     </div>
   );
 };
