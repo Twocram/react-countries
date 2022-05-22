348a4db62ba5ae75772a10de100bd024b85cc1e6
Twocram <2002maslovartem@gmail.com>
2022-05-22 12:12:48 +0500

    changed background-color

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,7 +24,7 @@ const Input = styled.input.attrs({
   margin-left: 2rem;
   border: none;
   outline: none;
-  background-color: var(--colors-bg);
+  background-color: var(--colors-ui-base);
   color: var(--color-text);
 `;
 
