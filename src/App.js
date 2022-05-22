dbb0e1bad39169764c2eeebeb2118615324c0fe1
Twocram <2002maslovartem@gmail.com>
2022-05-22 15:16:47 +0500

    added output of Card components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import { Main } from './components/Main';
 import { useState, useEffect } from 'react';
 import { ALL_COUNTRIES } from './config';
+import List from './components/List';
+import Card from './components/Card';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -20,6 +22,30 @@ function App() {
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: 'Population',
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: 'Region',
+                  description: c.region,
+                },
+                {
+                  title: 'Capital',
+                  description: c.capital,
+                },
+              ],
+            };
+
+            return <Card key={c.name} {...countryInfo} />;
+          })}
+        </List>
       </Main>
     </>
   );
