827c06e74ef9c5f7d38d95490082015f9d57e8c2
Twocram <2002maslovartem@gmail.com>
2022-05-22 10:14:19 +0500

    Search component basic structure

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { IoSearch } from 'react-icons/io5';
+
+const InputContainer = styled.label``;
+
+const Input = styled.input.attrs()``;
+
+const Search = ({ search, setSearch }) => {
+  return (
+    <InputContainer>
+      <IoSearch />
+      <Input onChange={(e) => setSearch(e.target.value)} value={search} />
+    </InputContainer>
+  );
+};
+
+export default Search;
