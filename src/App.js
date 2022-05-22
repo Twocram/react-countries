07ac82bdac89afae3695b408b0e68eef73df8e85
Twocram <2002maslovartem@gmail.com>
2022-05-22 16:14:51 +0500

    added routes for page navigation

--- src/App.js
+++ src/App.js
@@ -1,51 +1,20 @@
-import Controls from './components/Controls';
 import Header from './components/Header';
 import { Main } from './components/Main';
-import { useState, useEffect } from 'react';
-import { ALL_COUNTRIES } from './config';
-import List from './components/List';
-import Card from './components/Card';
 
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import NotFound from './pages/404';
 function App() {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
-  const fetchCountries = async () => {
-    const data = await fetch(ALL_COUNTRIES).then((data) => data.json());
-    setCountries(data);
-  };
   return (
     <>
       <Header />
       <Main>
-        <Controls />
-        <List>
-          {countries.map((c) => {
-            const countryInfo = {
-              img: c.flags.png,
-              name: c.name,
-              info: [
-                {
-                  title: 'Population',
-                  description: c.population.toLocaleString(),
-                },
-                {
-                  title: 'Region',
-                  description: c.region,
-                },
-                {
-                  title: 'Capital',
-                  description: c.capital,
-                },
-              ],
-            };
-
-            return <Card key={c.name} {...countryInfo} />;
-          })}
-        </List>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/country/:name' element={<Details />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       </Main>
     </>
   );
