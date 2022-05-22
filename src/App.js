092de85a505108cee85fd5d9d58769b4c713f140
Twocram <2002maslovartem@gmail.com>
2022-05-22 13:39:03 +0500

    created function for fetching countries from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import Controls from './components/Controls';
 import Header from './components/Header';
 import { Main } from './components/Main';
+import { useState, useEffect } from 'react';
+import { ALL_COUNTRIES } from './config';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    const data = await fetch(ALL_COUNTRIES).then((data) => data.json());
+    setCountries(data);
+  };
   return (
     <>
       <Header />
