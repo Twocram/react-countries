215ea7de2f45862d30021cfdd321bcb8988eccdc
Twocram <2002maslovartem@gmail.com>
2022-05-22 09:24:23 +0500

    Header elements styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,34 @@ import { IoMoon, IoMoonOutline } from "react-icons/io5";
 import { Container } from "./Container";
 import { useEffect, useState } from "react";
 
-const HeaderEl = styled.header``;
+const HeaderEl = styled.header`
+  box-shadow: var(--shadow);
+  background-color: var(--colors-ui-base);
+`;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
 
 const Title = styled.a.attrs({
   href: "/",
-})``;
+})`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  font-weight: var(--fw-bold);
+  text-decoration: none;
+`;
 
-const ModeSwitcher = styled.div``;
+const ModeSwitcher = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  font-weight: var(--fw-bold);
+  text-transform: capitalize;
+`;
 
 const Header = () => {
   const [theme, setTheme] = useState("light");
